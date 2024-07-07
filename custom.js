c971b1a3249d119e79becf99ee42b7f52f3ed94d
tobiecameron <tobie@lemonade.co.nz>
2024-07-07 14:18:34 +1200

    push codebase

---
+++ custom.js
@@ -0,0 +1,50 @@
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+  // gsap code here!
+});
+
+// Early JS calls
+$(document).ready(function () {
+  // $('html, body').animate({scrollTop: $(window.location).top}, 0);
+});
+
+// After DOM Calls
+window.onload = function () {
+    imageVideoswap();
+};
+
+
+
+
+
+// Creaet an array for all film objects to be added
+let imageFilm = [];
+
+
+function imageVideoswap() {
+
+
+    let hero = $("img[src*=-hero]");
+    console.log(hero);
+
+    $.each(hero, function (index, val) {
+   
+    });
+
+
+    // code = $(this).attr('id');
+    // ids.push(code);
+
+
+//   $.each(classLoop, function (index, val) {
+//     var a = $(val).find("a").eq(1);
+//     // console.log(a)
+//     var b = $(a).attr("href");
+//     // console.log(b)
+//     var c = "/" + b;
+//     // console.log(c)
+//     $(a).prop("href", c);
+//   });
+}
+

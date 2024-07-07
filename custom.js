5f3834fc8232e7f71d1fd555b843f26fa8e9e22a
tobiecameron <tobie@lemonade.co.nz>
2024-07-07 20:15:54 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -58,7 +58,7 @@ $(heroParent).each(function (i, obj) {
     //   if (!currentHero.paused) {
     //     // gsap.to(currentHero ".hero-video", .5, {opacity: 1});
     //     // gsap.to(currentHero toRemove, .5, {opacity: 0, delay: .5});
-    //     $(toRemove).hide();
+        $(heroImage).hide();
 
     //     console.log("PLAYING")
     //     clearInterval(time);

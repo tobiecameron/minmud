7e557a3f506a90d7e75ba6b9c22df76257e52631
tobiecameron <tobie@here.net.au>
2024-07-16 21:51:29 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 $(document).ready(function () {
   console.log("------- M I N M U D 2beta");
   // $('html, body').animate({scrollTop: $(window.location).top}, 0);
-//   imageVideoswap();
+  //   imageVideoswap();
   setTimeout(imageVideoswap(), 2000);
 });
 
@@ -17,8 +17,13 @@ $(document).ready(function () {
 //     imageVideoswap();
 // };
 
+
+
 // Creaet an array for all film objects to be added
 let imageFilm = [];
+
+
+
 
 function imageVideoswap() {
   let heroParent = $('img[src$="-hero.jpg"]').parent();
@@ -26,59 +31,31 @@ function imageVideoswap() {
 
   let i = 1;
 
-//   $.each(heroParent, function (index, val) {
-$(heroParent).each(function (i, obj) {
-
-    // var toRemove = $(this).find("img");
+  $(heroParent).each(function (i, obj) {
 
     let heroImage = $(heroParent).children().first();
-    // console.log(heroImage);
-    // $(heroImage).hside();
 
-    // ...
-
-
-    // console.log("to remove" + toRemove);
-
+    //replace the source jpg with the MP4 hosted on Vercel via GIT
     var src = $(this).find("img").attr("src").split("/");
     var hero = src[src.length - 1];
     hero = hero.replace("jpg", "mp4");
+    let filmHero = '<video class="hero-video" controls="true" muted autoplay playsinline="" preload="auto" loop="false" style="opacity: 1;"><source id="videoMP4" src="https://minmud.vercel.app/film/' + hero + '" type="video/mp4"></video>';
 
-    let filmHero = '<video class="hero-video" controls="true" muted autoplay playsinline="" preload="auto" loop="false" style="opacity: 1;"><source id="videoMP4" src="https://minmud.vercel.app/film/' + hero +'" type="video/mp4"></video>';
-
-    // attach video to image parent
+    // add video to the source images parent div
     $(this).prepend(filmHero);
-    let thisVid = $(this).find("img");
-    let currentHero = thisVid.add( hero );
-    
-    
-    // toRemove = thisVid.add( toRemove ); 
+    let currImage = $(this).find("img");
+
 
     console.log("currentHero" + currentHero);
 
     time = setInterval(function () {
       if (!currentHero.paused) {
-        // gsap.to(currentHero ".hero-video", .5, {opacity: 1});
-        // gsap.to(currentHero toRemove, .5, {opacity: 0, delay: .5});
-        thisVid.hide();
-
         console.log("PLAYING")
+        currImage.hide();
         clearInterval(time);
       }
     }, 500);
 
   });
 
-  // code = $(this).attr('id');
-  // ids.push(code);
-
-  //   $.each(classLoop, function (index, val) {
-  //     var a = $(val).find("a").eq(1);
-  //     // console.log(a)
-  //     var b = $(a).attr("href");
-  //     // console.log(b)
-  //     var c = "/" + b;
-  //     // console.log(c)
-  //     $(a).prop("href", c);
-  //   });
 }

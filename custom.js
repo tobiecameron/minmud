a4772258119c77d62751016f8463fd278f9f40e9
tobiecameron <tobie@here.net.au>
2024-07-16 22:19:50 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -36,11 +36,11 @@ function imageVideoswap() {
     let heroImage = $(heroParent).children().first();
 
     //replace the source jpg with the MP4 hosted on Vercel via GIT
-    var src = $(this).find("img").attr("src").split("/");
-    var Filename = src.pop();
+    let src = $(this).find("img").attr("src").split("/");
+    let Filename = src.pop();
     console.log(Filename);
 
-    var hero = src[src.length - 1];
+    let hero = src[src.length - 1];
     hero = hero.replace("jpg", "mp4");
     let filmHero = '<video class="hero-video" controls="true" muted autoplay playsinline="" preload="auto" loop="false" style="opacity: 1;"><source id="videoMP4" src="https://minmud.vercel.app/film/' + hero + '" type="video/mp4"></video>';
 

19864dcc334540956591e3eb9f629115df459961
tobiecameron <tobie@here.net.au>
2024-07-28 11:10:54 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -102,27 +102,23 @@ function sectionVideoControls() {
       //   }
       // });
     
-
-
-
-
-      // var playButton = document.getElementById("play_button");
-      // // Event listener for the play/pause button
-      // playButton.addEventListener("click", function() {
-      //   if (video.paused == true) {
-      //     // Play the video
-      //     video.play();
+      var playButton = document.getElementById("thisplayControl");
+      // Event listener for the play/pause button
+      playButton.addEventListener("click", function() {
+        if (currVideo.paused == true) {
+          // Play the video
+          currVideo.play();
       
-      //     // Update the button text to 'Pause'
-      //     playButton.innerHTML = "Pause";
-      //   } else {
-      //     // Pause the video
-      //     video.pause();
+          // Update the button text to 'Pause'
+          playButton.innerHTML = "Pause";
+        } else {
+          // Pause the video
+          currVideo.pause();
       
-      //     // Update the button text to 'Play'
-      //     playButton.innerHTML = "Play";
-      //   }
-      // });
+          // Update the button text to 'Play'
+          playButton.innerHTML = "Play";
+        }
+      });
 
 
     }

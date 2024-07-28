9b58c4220171db9349975494cbc5130dcaea1b40
tobiecameron <tobie@here.net.au>
2024-07-28 16:17:56 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -45,6 +45,7 @@ function imageVideoswap() {
 
     //replace the source jpg with the MP4 hosted on Vercel via GIT
     let src = $(this).find("img", "thumb-image" ).attr("src").split("/");
+    // comment here
 
     // let src = $(this).closest("img").attr("src").split("/");
     

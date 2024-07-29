f51bd9291cb146b8cfdb4b32f3cbfcd14f2083a9
tobiecameron <tobie@here.net.au>
2024-07-29 20:31:06 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -61,7 +61,7 @@ function imageVideoswap() {
 
     $(currVideo).on({
       play: function() {
-        $(this).removeClass('loaded');
+        $(this).currImage.removeClass('loaded');
         gsap.to(currImage, .5, { opacity: 0});
         // gsap.to(currImage, .5, { display: "none"});
       }

2dd24125962dd5a4d9d9e7521e9024cc5d98621e
tobiecameron <tobie@here.net.au>
2024-07-30 23:48:04 +1200

    golly

--- customSOLID.js
+++ customSOLID.js
@@ -9,8 +9,7 @@ $(document).ready(function () {
   console.log("------- M I N M U D 2beta");
 
   if ( window.frameElement == null ) {
-    imageSectionvideoswap();
-    imageGalleryvideoswap();
+    imageVideoswap();
   }
 });
 
@@ -20,116 +19,10 @@ $(document).ready(function () {
 // };
 
 
-
-function imageSectionvideoswap() {
+function imageVideoswap() {
 
   let heroParent = $('img[data-image$="-hero.jpg"]').parent();
   let i = 1;
-
-  $(heroParent).each(function (i, obj) {
-
-    if ($(this).parent().hasClass('section-background')) {
-
-      i++;
-      //replace the source jpg with the MP4 hosted on Vercel via GIT
-      let src = $(this).find("img").attr("data-src").split("/");
-      // console.log(src);
-
-      let hero = src[src.length - 1];
-      hero = hero.replace("jpg", "mp4");
-
-      let videoControl = "videoControl" + i;
-      let filmHero = '<video id="' + videoControl + '" class="hero-video ' + videoControl + '" muted autoplay playsinline="" preload="auto" loop="false" style="-webkit-border-radius: 1px; opacity: 1; object-fit: cover; object-position: 50% 50%;"><source id="videoMP4" src="https://minmud.vercel.app/film/' + hero + '" type="video/mp4"></video>';
-
-      // add video to the source images parent div
-      $(this).prepend(filmHero);
-      let currVideo = $(this).find("video");
-      let currImage = $(this).find("img");
-
-      $(currVideo).on({
-        play: function () {
-          $(currImage).removeClass('loaded');
-          gsap.to(currImage, .5, { opacity: 0, delay: 1 });
-          gsap.set(currImage, { display: "none", delay: 1.5 });
-        }
-      });
-
-      console.log("VIDEO FULL BACKGROUND DETECTED")
-      //  define the overlay elements
-
-      var overlayDiv;
-      var overlayDivContent;
-      var thisOverlay;
-
-      /* For sections wiht B A C K G R O U N D  images */
-      // if ($(this).find('.hero-video')) {
-        console.log("HAS SECTION BACKGROUND CLASS" + i);
-        overlayDiv = $(this).parent().siblings().first();
-        overlayDivContent = $(this).parent().siblings().first().find(".content");
-        thisOverlay = $(this).find(".section-background-overlay");
-      // }
-
-      /* For gallery blocks with images */
-      // if ($(this).parent().hasClass('margin-wrapper')){
-        // overlayDiv = $(this).parent().siblings().first();
-        // overlayDivContent = $(this).parent().siblings().first().find(".content");
-        // thisOverlay = $(this).find(".section-background-overlay");
-      // }
-      let thisshowControl = "show" + i;
-      let thishideControl = "hide" + i;
-      
-      let thisoverlayDivContent = overlayDivContent + x;
-
-      $(overlayDiv).append(
-        '<img id="'+ thisshowControl +'" class="showControl ' + thisshowControl + '" src="https://minmud.vercel.app/controls/show.svg"><img id="'+ thishideControl +'" class="hideControl ' + thishideControl + '" src="https://minmud.vercel.app/controls/hide.svg">'
-      );
-      
-      gsap.to('.' + thisshowControl, .25, { opacity: .7, display:"block", delay: 1});
-
-      var showButton = document.getElementById(thisshowControl);
-      var hideButton = document.getElementById(thishideControl);
-      var vid = document.getElementById('videoControl' + i);
-      // var allVids = document.getElementById('video');
-
-      showButton.addEventListener("click", function() {
-        console.log("SHOW LISTENER ACTIVATE" + i);
-        if (vid.paused == false) {
-          // Reveal the video
-          $('body video').each(function(){
-            /*** Do it here globally ***/
-            $(this).prop('muted', true);
-         });
-          vid.muted = false;
-          gsap.to([overlayDivContent, thisOverlay], .25, { opacity: 0});
-          gsap.to("." + thisshowControl, .25, { opacity: 0, display:"none"});
-          gsap.to("." + thishideControl, .25, { opacity: .7, display:"block", delay: .25});
-        }
-      });
-
-      hideButton.addEventListener("click", function() {
-        if (vid.paused == false) {
-          // Reveal the video
-          vid.muted = true;
-          gsap.to(thisOverlay, .25, { opacity: .55});
-          gsap.to(overlayDivContent, .25, { opacity: 1});
-          gsap.to("." + thishideControl, .25, { opacity: 0, display:"none"});
-          gsap.to("." + thisshowControl, .25, { opacity: .7, display:"block", delay: .25});
-        }
-      });
-
-
-
-
-    }
-  });
-  // setTimeout(sectionVideoControls, 250)
-}
-
-
-function imageGalleryvideoswap() {
-
-  let heroParent = $('img[data-image$="-hero.jpg"]').parent();
-  let i = 0;
 
   $(heroParent).each(function (i, obj) {
     i++;
@@ -176,9 +69,101 @@ function imageGalleryvideoswap() {
         gsap.set(currImage, { display: "none", delay: 1.5});
       }
     });
-
+    sectionVideoControls();
   });
 }
+
+
+
+
+// ADD VIDEO CONTROLS TO BACKGROUND VIDEOS
+function sectionVideoControls() {
+  let x = 0;
+
+  $(".section-background").each(function (i, obj) {
+    if ($(this).find('.hero-video').length !== 0) {
+      x++
+      console.log("VIDEO FULL BACKGROUND DETECTED")
+      //  define the overlay elements
+
+      var overlayDiv;
+      var overlayDivContent;
+      var thisOverlay;
+
+      /* For sections wiht B A C K G R O U N D  images */
+      // if ($(this).find('.hero-video')) {
+        console.log("HAS SECTION BACKGROUND CLASS" + x);
+        overlayDiv = $(this).parent().siblings().first();
+        overlayDivContent = $(this).parent().siblings().first().find(".content");
+        thisOverlay = $(this).find(".section-background-overlay");
+      // }
+
+      /* For gallery blocks with images */
+      // if ($(this).parent().hasClass('margin-wrapper')){
+        // overlayDiv = $(this).parent().siblings().first();
+        // overlayDivContent = $(this).parent().siblings().first().find(".content");
+        // thisOverlay = $(this).find(".section-background-overlay");
+      // }
+      let thisshowControl = "show" + x;
+      let thishideControl = "hide" + x;
+      
+      let thisoverlayDivContent = overlayDivContent + x;
+
+      $(overlayDiv).append(
+        '<img id="'+ thisshowControl +'" class="showControl ' + thisshowControl + '" src="https://minmud.vercel.app/controls/show.svg"><img id="'+ thishideControl +'" class="hideControl ' + thishideControl + '" src="https://minmud.vercel.app/controls/hide.svg">'
+      );
+      
+      gsap.to('.' + thisshowControl, .25, { opacity: .7, display:"block", delay: 1});
+
+      var showButton = document.getElementById(thisshowControl);
+      var hideButton = document.getElementById(thishideControl);
+      var vid = document.getElementById('videoControl' + x);
+      // var allVids = document.getElementById('video');
+
+      showButton.addEventListener("click", function() {
+        console.log("SHOW LISTENER ACTIVATE" + x);
+        if (vid.paused == false) {
+          // Reveal the video
+          $('body video').each(function(){
+            /*** Do it here globally ***/
+            $(this).prop('muted', true);
+         });
+          vid.muted = false;
+          gsap.to([overlayDivContent, thisOverlay], .25, { opacity: 0});
+          gsap.to("." + thisshowControl, .25, { opacity: 0, display:"none"});
+          gsap.to("." + thishideControl, .25, { opacity: .7, display:"block", delay: .25});
+        }
+      });
+
+      hideButton.addEventListener("click", function() {
+        if (vid.paused == false) {
+          // Reveal the video
+          vid.muted = true;
+          gsap.to(thisOverlay, .25, { opacity: .55});
+          gsap.to(overlayDivContent, .25, { opacity: 1});
+          gsap.to("." + thishideControl, .25, { opacity: 0, display:"none"});
+          gsap.to("." + thisshowControl, .25, { opacity: .7, display:"block", delay: .25});
+        }
+      });
+
+      // showButton.addEventListener("click", function() {
+      //   if (vid.paused == false) {
+      //     // Reveal the video
+      //     vid.muted = false;
+      //     gsap.to(thisoverlayDivContent, .5, { opacity: 0});
+      //   }
+        
+      //   if (vid.muted == false) {
+      //     // Play the video
+      //     vid.play();
+      //     vid.muted = false;      
+      //   }
+      // });
+    }
+  });
+}
+
+
 
 
 

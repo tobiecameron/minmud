ff40639a39c378a9db66fc03e82fa80b25b3c5ec
tobiecameron <tobie@here.net.au>
2024-07-31 21:15:33 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 $(document).ready(function () {
   console.log("------- M I N M U D 2beta");
 
-  if ( window.frameElement == null ) {
+  if (window.frameElement == null) {
     imageSectionvideoswap();
 
   }
@@ -35,8 +35,6 @@ function imageSectionvideoswap() {
 
       //replace the source jpg with the MP4 hosted on Vercel via GIT
       let src = $(this).find("img").attr("data-src").split("/");
-      // console.log(src);
-
       let hero = src[src.length - 1];
       hero = hero.replace("jpg", "mp4");
 
@@ -60,75 +58,63 @@ function imageSectionvideoswap() {
       });
 
       console.log("VIDEO FULL BACKGROUND DETECTED")
-      //  define the overlay elements
 
       var overlayDiv;
       var overlayDivContent;
       var thisOverlay;
 
-      /* For sections wiht B A C K G R O U N D  images */
-      // if ($(this).find('.hero-video')) {
-        console.log("HAS SECTION BACKGROUND CLASS" + i);
-        overlayDiv = $(this).parent().siblings().first();
-        overlayDivContent = $(this).parent().siblings().first().find(".content");
-        thisOverlay = $(this).find(".section-background-overlay");
-      // }
+      console.log("HAS SECTION BACKGROUND CLASS" + i);
+      $(this).parent().siblings().first().addClass('sectionOverlay');
+      overlayDiv = $(this).parent().siblings().first();
 
-      /* For gallery blocks with images */
-      // if ($(this).parent().hasClass('margin-wrapper')){
-        // overlayDiv = $(this).parent().siblings().first();
-        // overlayDivContent = $(this).parent().siblings().first().find(".content");
-        // thisOverlay = $(this).find(".section-background-overlay");
-      // }
+      overlayDivContent = $(this).parent().siblings().first().find(".content");
+      thisOverlay = $(this).find(".section-background-overlay");
+
       let thisshowControl = "show" + i;
       let thishideControl = "hide" + i;
-      
+
       let thisoverlayDivContent = overlayDivContent + i;
 
       $(overlayDiv).append(
-        '<img id="'+ thisshowControl +'" class="showControl ' + thisshowControl + '" src="https://minmud.vercel.app/controls/show.svg"><img id="'+ thishideControl +'" class="hideControl ' + thishideControl + '" src="https://minmud.vercel.app/controls/hide.svg">'
+        '<img id="' + thisshowControl + '" class="showControl ' + thisshowControl + '" src="https://minmud.vercel.app/controls/show.svg"><img id="' + thishideControl + '" class="hideControl ' + thishideControl + '" src="https://minmud.vercel.app/controls/hide.svg">'
       );
-      
-      gsap.to('.' + thisshowControl, .25, { opacity: .7, display:"block", delay: 1});
+
+      gsap.to('.' + thisshowControl, .25, { opacity: .7, display: "block", delay: 1 });
 
       var showButton = document.getElementById(thisshowControl);
       var hideButton = document.getElementById(thishideControl);
       var vid = document.getElementById('videoControl' + i);
       // var allVids = document.getElementById('video');
 
-      showButton.addEventListener("click", function() {
+      showButton.addEventListener("click", function () {
         console.log("SHOW LISTENER ACTIVATE" + i);
         if (vid.paused == false) {
           // Reveal the video
-          $('body video').each(function(){
+          $('body video').each(function () {
             /*** Do it here globally ***/
             $(this).prop('muted', true);
-         });
+          });
           vid.muted = false;
-          gsap.to([overlayDivContent, thisOverlay], .25, { opacity: 0});
-          gsap.to("." + thisshowControl, .25, { opacity: 0, display:"none"});
-          gsap.to("." + thishideControl, .25, { opacity: .7, display:"block", delay: .25});
+          gsap.to('.sectionOverlay, .sectionOverlay .content', 0, { opacity: 1 });   
+          gsap.to([overlayDivContent, thisOverlay], .25, { opacity: 0 });
+          gsap.to("." + thisshowControl, .25, { opacity: 0, display: "none" });
+          gsap.to("." + thishideControl, .25, { opacity: .7, display: "block", delay: .25 });
         }
       });
 
-      hideButton.addEventListener("click", function() {
+      hideButton.addEventListener("click", function () {
         if (vid.paused == false) {
           // Reveal the video
           vid.muted = true;
-          gsap.to(thisOverlay, .25, { opacity: .55});
-          gsap.to(overlayDivContent, .25, { opacity: 1});
-          gsap.to("." + thishideControl, .25, { opacity: 0, display:"none"});
-          gsap.to("." + thisshowControl, .25, { opacity: .7, display:"block", delay: .25});
+          gsap.to(thisOverlay, .25, { opacity: .55 });
+          gsap.to(overlayDivContent, .25, { opacity: 1 });
+          gsap.to("." + thishideControl, .25, { opacity: 0, display: "none" });
+          gsap.to("." + thisshowControl, .25, { opacity: .7, display: "block", delay: .25 });
         }
       });
 
-
-
-
     }
   });
-  // setTimeout(sectionVideoControls, 250)
-
   imageGalleryvideoswap();
 }
 
@@ -139,50 +125,46 @@ function imageGalleryvideoswap() {
   let i = 0;
 
   $(heroParent).each(function (i, obj) {
-    if ($(this).parent().hasClass('margin-wrapper')){
-    i++;
-    //replace the source jpg with the MP4 hosted on Vercel via GIT
-    let src = $(this).find("img").attr("data-src").split("/");
-    // console.log(src);
-   
-    let hero = src[src.length - 1];
-    hero = hero.replace("jpg", "mp4");
+    if ($(this).parent().hasClass('margin-wrapper')) {
+      i++;
+      //replace the source jpg with the MP4 hosted on Vercel via GIT
+      let src = $(this).find("img").attr("data-src").split("/");
+      // console.log(src);
 
-    let videoControl = "videoControl" + i; 
-    let filmHero = '<video id="'+ videoControl +'" class="hero-video ' + videoControl + '" muted autoplay playsinline="" preload="auto" loop="false" style="-webkit-border-radius: 1px; opacity: 1; object-fit: cover; object-position: 50% 50%;"><source id="videoMP4" src="https://minmud.vercel.app/film/' + hero + '" type="video/mp4"></video>';
+      let hero = src[src.length - 1];
+      hero = hero.replace("jpg", "mp4");
 
-    // add video to the source images parent div
-    $(this).prepend(filmHero);
-    let currVideo = $(this).find("video");
-    let currImage = $(this).find("img");
+      let videoControl = "videoControl" + i;
+      let filmHero = '<video id="' + videoControl + '" class="hero-video ' + videoControl + '" muted autoplay playsinline="" preload="auto" loop="false" style="-webkit-border-radius: 1px; opacity: 1; object-fit: cover; object-position: 50% 50%;"><source id="videoMP4" src="https://minmud.vercel.app/film/' + hero + '" type="video/mp4"></video>';
+
+      // add video to the source images parent div
+      $(this).prepend(filmHero);
+      let currVideo = $(this).find("video");
+      let currImage = $(this).find("img");
 
 
 
-    ////////////* For the Gallery blocks */
+      ////////////* For the Gallery blocks */
 
-        $(this).parent().closest('div').addClass('galleryFilm');  //your function
+      $(this).parent().closest('div').addClass('galleryFilm');  //your function
 
-        let thisBackground = '<div class="gallery-overlay" style="transition-timing-function: ease; transition-duration: 0.9s; transition-delay: 0.229091s;"></div>';
+      let thisBackground = '<div class="gallery-overlay" style="transition-timing-function: ease; transition-duration: 0.9s; transition-delay: 0.229091s;"></div>';
 
-        let thisLink = $(this).attr('href');
-        console.log("the link from the gallery" + thisLink);
+      let thisLink = $(this).attr('href');
+      console.log("the link from the gallery" + thisLink);
 
-        let thisButton = '<div class="sqs-block-button-container sqs-block-button-container--left preFade fadeIn galleryButton" data-animation-role="button" data-alignment="left" data-button-size="medium" data-button-type="primary" id="yui_3_17_2_1_1722245060304_151" style="transition-timing-function: ease; transition-duration: 0.9s; transition-delay: 0.229091s;"><a href="' + thisLink + '" class="sqs-block-button-element--medium sqs-button-element--primary sqs-block-button-element" data-initialized="true">View Project</a></div>'
-        
-        // add video to the source images parent div
-        $(this).append(thisBackground);
-        $(this).parent().find('.image-slide-title').append(thisButton);
-        // $(this).append(thisButton);
-    /////////////* end gallery vlocks */
-    // console.log(currImage);
+      let thisButton = '<div class="sqs-block-button-container sqs-block-button-container--left preFade fadeIn galleryButton" data-animation-role="button" data-alignment="left" data-button-size="medium" data-button-type="primary" id="yui_3_17_2_1_1722245060304_151" style="transition-timing-function: ease; transition-duration: 0.9s; transition-delay: 0.229091s;"><a href="' + thisLink + '" class="sqs-block-button-element--medium sqs-button-element--primary sqs-block-button-element" data-initialized="true">View Project</a></div>'
 
-    $(currVideo).on({
-      play: function() {
-        $(currImage).removeClass('loaded');
-        gsap.to(currImage, .5, { opacity: 0, delay: 1});
-        gsap.set(currImage, { display: "none", delay: 1.5});
-      }
-    });
+      $(this).append(thisBackground);
+      $(this).parent().find('.image-slide-title').append(thisButton);
+
+      $(currVideo).on({
+        play: function () {
+          $(currImage).removeClass('loaded');
+          gsap.to(currImage, .5, { opacity: 0, delay: 1 });
+          gsap.set(currImage, { display: "none", delay: 1.5 });
+        }
+      });
     }
   });
 }

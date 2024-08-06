2750eff3af4d6c02d72dff1e5397a0e2128d7d32
tobiecameron <tobie@lemonade.co.nz>
2024-08-07 11:05:08 +1200

    Update custom.js

--- custom.js
+++ custom.js
@@ -203,7 +203,7 @@ function imageGalleryvideoswap() {
 
       
       // if ($(this).parent().eq(0).find('.image-slide-title')) {
-      if($(this).parent().find('.image-slide-title').length !== 0) {
+      if($(this).parent().children('div').find('.image-slide-title').length !== 0) {
         
         $(this).append(thisBackground);
       }
